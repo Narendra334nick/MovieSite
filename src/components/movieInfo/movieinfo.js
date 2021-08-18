90b986bed4fb14a507a33c77c1b1623c5e152b9d
nick <recs.cse1626@gmail.com>
2021-08-18 22:13:33 +0530

    code format

--- src/components/movieInfo/movieinfo.js
+++ src/components/movieInfo/movieinfo.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Loader from '../../globalComponents/KenLoader/index';
 
 const Movieinfo = (props) => {
-    const [movie,setMovie] = React.useState({});
-    const [loader,setLoader] = React.useState(true);
+  const [movie, setMovie] = React.useState({});
+  const [loader, setLoader] = React.useState(true);
 
-    const getMovieData = async () =>{
-        const id = sessionStorage.getItem('movieId');
-        const apiKey = 10425274;
-        const url = `https://www.omdbapi.com/?i=${id}&apikey=${apiKey}`
-        const result = await fetch(url)
-        const data = await result.json();
-        setMovie(data)
-        setLoader(false);
-    }
+  const getMovieData = async () => {
+    const id = sessionStorage.getItem('movieId');
+    const apiKey = 10425274;
+    const url = `https://www.omdbapi.com/?i=${id}&apikey=${apiKey}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    setMovie(data);
+    setLoader(false);
+  };
 
-    React.useEffect(()=>{
-        getMovieData();
-    },[]);
+  React.useEffect(() => {
+    getMovieData();
+  }, []);
 
   return (
     <div style={{ marginTop: '20px' }}>
-        {
-            loader && (<Loader/>)
-        }
+      {loader && <Loader />}
       <img src={movie.Poster} alt=""></img>
       <div>Name:{movie.Title}</div>
       <div>Actors:{movie.Actors}</div>
@@ -35,45 +33,4 @@ const Movieinfo = (props) => {
     </div>
   );
 };
-
-// export class movieinfo extends Component {
-//     constructor(props) {
-//         super(props)
-
-//         this.state = {
-//             movie:{},
-//             loader:false
-//         }
-//     }
-
-//     async componentDidMount(){
-//         const id = sessionStorage.getItem('movieId');
-//         const apiKey = 10425274;
-//         const url = `https://www.omdbapi.com/?i=${id}&apikey=${apiKey}`
-//         const result = await fetch(url)
-//         const data = await result.json();
-//         //console.log(data);
-
-//         this.setState({
-//             movie:data
-//         })
-//         //console.log(this.state);
-//     }
-
-//     render() {
-//         return (
-//             <div style={{marginTop:"20px"}}>
-//                 <img src={this.state.movie.Poster} alt=""></img>
-//                 <div>Name:{this.state.movie.Title}</div>
-//                 <div>Actors:{this.state.movie.Actors}</div>
-//                 <div>Director:{this.state.movie.Director}</div>
-//                 <div>Genere:{this.state.movie.Genre}</div>
-//                 <div>Language:{this.state.movie.Language}</div>
-//                 <div>Year:{this.state.movie.Year}</div>
-//                 <div>Type:{this.state.movie.Type}</div>
-//             </div>
-//         )
-//     }
-// }
-
 export default Movieinfo;
